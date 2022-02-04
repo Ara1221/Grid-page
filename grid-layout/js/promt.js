a12138966b8682acd68749201cd06d163b4a35c9
Aram <aramakopyan23@gmail.com>
2022-02-04 21:25:55 +0300

    add a little bit a JavaScript

---
+++ grid-layout/js/promt.js
@@ -0,0 +1,13 @@
+let modal = prompt('If you solve a math problem, you will get a 5% sale on your order.' +
+    ' 2+2*6-4');
+
+if (modal == 10) {
+    alert('Cool! enter the promo code "q23efgtre17a" and receive a 5% sale.');
+}
+
+else if (modal < 10){
+    alert('Unfortunately it is wrong answer');
+}
+else if (modal > 10){
+    alert('Unfortunately it is wrong answer');
+}
